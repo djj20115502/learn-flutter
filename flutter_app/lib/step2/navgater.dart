e8df05192ca40e8ce54e5b2068319db88fe2e1d9
djj <dongjunjie.mail@qq.com>
2019-07-17 15:04:11 +0800

    feat(viewpager)：类似viewpager的页面，这里发现其实flutter的也面试全屏的， 后续研究页面具体的布局逻辑

--- flutter_app/lib/step2/navgater.dart
+++ flutter_app/lib/step2/navgater.dart
@@ -6,41 +6,73 @@ class BottomNavgater extends StatefulWidget {
 }
 
 class _State extends State<BottomNavgater> {
+  int index=0;
+  List<Widget> list = List();
+  @override
+  void initState() {
+    list
+      ..add(Detail(title: "Home"))
+      ..add(Detail(title: "Email"))
+      ..add(Detail(title: "Pages"))
+      ..add(Detail(title: "AipPlay"))
+      ..add(Detail(title: "home5"));
+
+    super.initState();
+  }
+
   final _BottomNavigationColor = Colors.blue;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-    
-      bottomNavigationBar: BottomNavigationBar(items: [
-        BottomNavigationBarItem(
-            icon: Icon(
-              Icons.home,
-              color: _BottomNavigationColor,
-            ),
-            title:
-                Text('Home', style: TextStyle(color: _BottomNavigationColor))),
-        BottomNavigationBarItem(
-            icon: Icon(
-              Icons.email,
-              color: _BottomNavigationColor,
-            ),
-            title:
-                Text('Email', style: TextStyle(color: _BottomNavigationColor))),
-        BottomNavigationBarItem(
-            icon: Icon(
-              Icons.pages,
-              color: _BottomNavigationColor,
-            ),
-            title:
-                Text('Pages', style: TextStyle(color: _BottomNavigationColor))),
-        BottomNavigationBarItem(
-            icon: Icon(
-              Icons.airplay,
-              color: _BottomNavigationColor,
-            ),
-            title: Text('AipPlay',
-                style: TextStyle(color: _BottomNavigationColor))),
-      ], type: BottomNavigationBarType.fixed),
+      body: list[index],
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(
+              icon: Icon(
+                Icons.home,
+                color: _BottomNavigationColor,
+              ),
+              title: Text('Home',
+                  style: TextStyle(color: _BottomNavigationColor))),
+          BottomNavigationBarItem(
+              icon: Icon(
+                Icons.email,
+                color: _BottomNavigationColor,
+              ),
+              title: Text('Email',
+                  style: TextStyle(color: _BottomNavigationColor))),
+          BottomNavigationBarItem(
+              icon: Icon(
+                Icons.pages,
+                color: _BottomNavigationColor,
+              ),
+              title: Text('Pages',
+                  style: TextStyle(color: _BottomNavigationColor))),
+          BottomNavigationBarItem(
+              icon: Icon(
+                Icons.airplay,
+                color: _BottomNavigationColor,
+              ),
+              title: Text('AipPlay',
+                  style: TextStyle(color: _BottomNavigationColor))),
+        ],
+        type: BottomNavigationBarType.fixed,
+        onTap: (i) => {
+              setState(() => index = i)
+            },
+      ),
+    );
+  }
+}
+
+class Detail extends StatelessWidget {
+  final String title;
+  Detail({Key key, this.title}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      body: Text(title),
     );
   }
 }
