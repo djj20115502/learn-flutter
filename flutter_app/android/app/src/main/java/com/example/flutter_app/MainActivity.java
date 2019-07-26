e2e48fd26ae4e74fc06fe88a5a6086093217842a
djj <dongjunjie.mail@qq.com>
2019-07-26 10:51:41 +0800

    feat(线程)： 1 所谓的await只是抛到了队列后面执行，相当于view.post。其实还是在主线程中。要想弄个耗时的，这里要新开isolate 2 引入rxjava，统一在Android端处理相关的回调

--- flutter_app/android/app/src/main/java/com/example/flutter_app/MainActivity.java
+++ flutter_app/android/app/src/main/java/com/example/flutter_app/MainActivity.java
@@ -1,8 +1,7 @@
 package com.example.flutter_app;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.HandlerThread;
 import android.util.Log;
 
 import java.io.File;
@@ -16,14 +15,19 @@ import io.flutter.app.FlutterActivity;
 import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugins.GeneratedPluginRegistrant;
+import io.reactivex.Observable;
+import io.reactivex.ObservableEmitter;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
 import jxl.Cell;
 import jxl.Sheet;
 import jxl.Workbook;
 import jxl.read.biff.BiffException;
 
 public class MainActivity extends FlutterActivity {
-    HandlerThread handlerThread;
-    Handler handler;
+
+    final ArrayList<Disposable> disposables = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,53 +35,71 @@ public class MainActivity extends FlutterActivity {
         Log.e("FlutterActivity", "onMethodCall"
                 + Thread.currentThread().getName()
                 + Thread.currentThread().getId());
+        GeneratedPluginRegistrant.registerWith(this);
 
-        handlerThread = new HandlerThread("handlerThread");
-        handlerThread.start();
-        handler = new Handler(handlerThread.getLooper());
-        handler.post(() -> {
-                    Log.e("FlutterActivity", "post  "
+        new MethodChannel(getFlutterView(), CHANNEL).setMethodCallHandler(this::onMethodCall);
+    }
+
+    @SuppressLint("CheckResult")
+    private void onMethodCall(MethodCall call, MethodChannel.Result result) {
+
+        Disposable disposable = Observable.create(
+                (ObservableEmitter<Ans> emitter) -> {
+                    Log.e("FlutterActivity", "ObservableEmitter"
                             + Thread.currentThread().getName()
                             + Thread.currentThread().getId());
-                }
-        );
+                    Ans ans = new Ans();
+                    ans.code = 0;
+                    switch (call.method) {
+                        case JUST_TEST:
+                            ans.code = 1;
+                            ans.data = "just_testdddddddddd";
+                            break;
+                        case XLS:
+                            ans.code = 1;
+                            ans.data = analyzeXls(getFilesDir().getParent() + "/app_flutter/right.x1s");
+                            break;
+                        default:
+                            ans.code = -1;
 
-        GeneratedPluginRegistrant.registerWith(this);
-        new MethodChannel(getFlutterView(), CHANNEL).setMethodCallHandler(
-                (MethodCall call, MethodChannel.Result result) -> {
-                    handler.post(() -> {
-                        Log.e("FlutterActivity", "onMethodCall"
-                                + call.method
-                                + Thread.currentThread().getName()
-                                + Thread.currentThread().getId());
-                        try {
-                            Thread.sleep(1000*10);
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
-                        }
-                        MainActivity.this.runOnUiThread(new Runnable() {
-                            @Override
-                            public void run() {
-                                switch (call.method) {
-                                    case JUST_TEST:
-                                        result.success("just_testdddddddddd");
-                                        return;
-                                    case XLS:
-                                        result.success(analyzeXls(getFilesDir().getParent() + "/app_flutter/right.x1s"));
-                                        return;
-                                }
-                                result.notImplemented();
-                            }
-                        });
-                    });
-                }
-        );
+                    }
+                    emitter.onNext(ans);
+                    emitter.onComplete();
+                })
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeOn(Schedulers.io())
+                .subscribe((Ans o) -> {
+                    switch (o.code) {
+                        case -1:
+                            result.notImplemented();
+                            break;
 
+                        case 0:
+                            result.error("", "", "");
+                            break;
+                        case 1:
+                            result.success(o.data);
+                    }
+                });
+        disposables.add(disposable);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        for (Disposable disposable : disposables) {
+            disposable.isDisposed();
+        }
     }
 
     private static final String CHANNEL = "samples.flutter.io";
     private static final String XLS = "xls";
     private static final String JUST_TEST = "just_test";
+
+    class Ans {
+        int code; //-1 没有方法 0 有方法没有 错误 1 有返回结果
+        Object data;
+    }
 
     public Map<String, List<List<String>>> analyzeXls(String fileName) {
         Map<String, List<List<String>>> map = new HashMap<>();
