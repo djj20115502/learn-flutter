a8f8d7d2b40ff4f0d0b93411dfd65b05eb555e60
djj <dongjunjie.mail@qq.com>
2019-12-10 17:06:56 +0800

    feat(flutter):list方式不行，但是直接摆上去可以

--- flutter_app/lib/cattleclass/showlist.dart
+++ flutter_app/lib/cattleclass/showlist.dart
@@ -34,93 +34,117 @@ class _ShowAllStudentState extends State<ShowAllStudent> {
 
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
-        home: new Scaffold(
-      appBar: new AppBar(
-        title: new Text("LinearLayout Example"),
-      ),
-      body: Scrollbar(
-        child: SingleChildScrollView(
-          scrollDirection: Axis.horizontal,
-          child: Container(
-            color: Colors.yellowAccent,
-            child: new Row(
-              //Column
-              mainAxisSize: MainAxisSize.min,
-              //wrap_content ,不加的话默认为match_parent（MainAxisSize.max）
-//              mainAxisSize: MainAxisSize.max,
-              mainAxisAlignment: MainAxisAlignment.start,
-              //start==left,center==center,end==right ,
-              // spaceEvenly==等比例居中，4个间距一样大（weight=1),spaceAround=等比例居中，6个间距一样大,spaceBetween=中间居中，两边顶边
-              children: [
-                Container(
-                  child: new Icon(
-                    Icons.access_time,
-                    size: 50.0,
-                  ),
-                  color: Colors.red,
-                ),
-                Container(
-                  child: new Icon(
-                    Icons.pie_chart,
-                    size: 370.0,
-                  ),
-                  color: Colors.blue,
-                ),
-                Container(
-                  child: new Icon(
-                    Icons.email,
-                    size: 50.0,
-                  ),
-                  color: Colors.green,
-                ),
-              ],
-            ),
-          ),
-        ),
-      ),
-    ));
-
-//    return Scaffold(
-//      appBar: AppBar(
-//        // Here we take the value from the MyHomePage object that was created by
-//        // the App.build method, and use it to set our appbar title.
-//        title: Text("列表"),
+//    return   new Scaffold(
+//      appBar: new AppBar(
+//        title: new Text("LinearLayout Example"),
 //      ),
 //      body: Scrollbar(
 //        child: SingleChildScrollView(
 //          scrollDirection: Axis.horizontal,
 //          child: Container(
-//            width: 900,
-//            height: 500,
-//            child: ListView.builder(
-//                itemCount: table.length,
-//                itemExtent: 50.0, //强制高度为50.0
-//                itemBuilder: (BuildContext context, int index) {
-//                  return ListTile(title: Text("$index" + table[index]));
-//                }),
+//            color: Colors.yellowAccent,
+//            child: new Row(
+//              //Column
+//              mainAxisSize: MainAxisSize.min,
+//              //wrap_content ,不加的话默认为match_parent（MainAxisSize.max）
+////              mainAxisSize: MainAxisSize.max,
+//              mainAxisAlignment: MainAxisAlignment.start,
+//              //start==left,center==center,end==right ,
+//              // spaceEvenly==等比例居中，4个间距一样大（weight=1),spaceAround=等比例居中，6个间距一样大,spaceBetween=中间居中，两边顶边
+//              children: [
+//                Container(
+//                  child: new Icon(
+//                    Icons.access_time,
+//                    size: 50.0,
+//                  ),
+//                  color: Colors.red,
+//                ),
+//                Container(
+//                  child: new Icon(
+//                    Icons.pie_chart,
+//                    size: 370.0,
+//                  ),
+//                  color: Colors.blue,
+//                ),
+//                Container(
+//                  child: new Icon(
+//                    Icons.email,
+//                    size: 50.0,
+//                  ),
+//                  color: Colors.green,
+//                ),
+//              ],
+//            ),
 //          ),
 //        ),
 //      ),
-//    );
-  }
+//    ) ;
 
-  Widget _widget(int i) {
-    return RaisedButton(
-      color: Colors.blue,
-      highlightColor: Colors.blue[700],
-      colorBrightness: Brightness.dark,
-      splashColor: Colors.grey,
-      child: Text(table[i]),
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
-      onPressed: () => CommonUtils.showToast(context, table[i]),
-    );
+    return Scaffold(
+        appBar: AppBar(
+          // Here we take the value from the MyHomePage object that was created by
+          // the App.build method, and use it to set our appbar title.
+          title: Text("列表"),
+        ),
+        body: Scrollbar(
+          child: SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Column(
+                  //              child: ListView.builder(
+//                  itemCount: table.length,
+//                  itemExtent: 50.0, //强制高度为50.0
+//                  itemBuilder: (BuildContext context, int index) {
+//                    return _widget2(index);
+//                  })),
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [_widget2(1), _widget2(2)])),
+        ));
   }
 
   Widget _widget2(int i) {
-    return Row(children: <Widget>[
-      Text("第" + i.toString()),
-      Text(table[i]),
-    ]);
+    return Row(
+        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: <Widget>[
+          Text("第" + i.toString()),
+          Text(table[i] * 5),
+        ]);
+  }
+
+  Widget _widget24() {
+    return Container(
+        color: Colors.yellowAccent,
+        child: new Row(
+            //Column
+            mainAxisSize: MainAxisSize.min,
+            //wrap_content ,不加的话默认为match_parent（MainAxisSize.max）
+//              mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.start,
+            //start==left,center==center,end==right ,
+            // spaceEvenly==等比例居中，4个间距一样大（weight=1),spaceAround=等比例居中，6个间距一样大,spaceBetween=中间居中，两边顶边
+            children: [
+              Container(
+                child: new Icon(
+                  Icons.access_time,
+                  size: 50.0,
+                ),
+                color: Colors.red,
+              ),
+              Container(
+                child: new Icon(
+                  Icons.pie_chart,
+                  size: 370.0,
+                ),
+                color: Colors.blue,
+              ),
+              Container(
+                child: new Icon(
+                  Icons.email,
+                  size: 50.0,
+                ),
+                color: Colors.green,
+              ),
+            ]));
   }
 }
