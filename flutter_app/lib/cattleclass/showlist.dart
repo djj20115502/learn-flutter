d6dacf271f9469a41b35a4f1cb491dcb447f90f5
djj <dongjunjie.mail@qq.com>
2019-12-10 16:20:12 +0800

    feat(flutter):相当于warpcontent了

--- flutter_app/lib/cattleclass/showlist.dart
+++ flutter_app/lib/cattleclass/showlist.dart
@@ -34,39 +34,75 @@ class _ShowAllStudentState extends State<ShowAllStudent> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text("列表"),
+    return new MaterialApp(
+        home: new Scaffold(
+      appBar: new AppBar(
+        title: new Text("LinearLayout Example"),
       ),
       body: Scrollbar(
         child: SingleChildScrollView(
           scrollDirection: Axis.horizontal,
           child: Container(
-            width: 900,
-            height: 500,
-
-            child: GridView(
-              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 1,
-                crossAxisSpacing: 20,
-
-                ///横轴间距
-                mainAxisSpacing: 1,
-
-                ///纵轴间距
-//                 childAspectRatio:1 ,///宽高比
-              ),
-              children: List<Widget>.generate(
-                table.length,
-                _widget2,
-              ),
+            color: Colors.yellowAccent,
+            child: new Row(
+              //Column
+              mainAxisSize: MainAxisSize.min,
+              //wrap_content ,不加的话默认为match_parent（MainAxisSize.max）
+//              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.start,
+              //start==left,center==center,end==right ,
+              // spaceEvenly==等比例居中，4个间距一样大（weight=1),spaceAround=等比例居中，6个间距一样大,spaceBetween=中间居中，两边顶边
+              children: [
+                Container(
+                  child: new Icon(
+                    Icons.access_time,
+                    size: 50.0,
+                  ),
+                  color: Colors.red,
+                ),
+                Container(
+                  child: new Icon(
+                    Icons.pie_chart,
+                    size: 370.0,
+                  ),
+                  color: Colors.blue,
+                ),
+                Container(
+                  child: new Icon(
+                    Icons.email,
+                    size: 50.0,
+                  ),
+                  color: Colors.green,
+                ),
+              ],
             ),
           ),
         ),
       ),
-    );
+    ));
+
+//    return Scaffold(
+//      appBar: AppBar(
+//        // Here we take the value from the MyHomePage object that was created by
+//        // the App.build method, and use it to set our appbar title.
+//        title: Text("列表"),
+//      ),
+//      body: Scrollbar(
+//        child: SingleChildScrollView(
+//          scrollDirection: Axis.horizontal,
+//          child: Container(
+//            width: 900,
+//            height: 500,
+//            child: ListView.builder(
+//                itemCount: table.length,
+//                itemExtent: 50.0, //强制高度为50.0
+//                itemBuilder: (BuildContext context, int index) {
+//                  return ListTile(title: Text("$index" + table[index]));
+//                }),
+//          ),
+//        ),
+//      ),
+//    );
   }
 
   Widget _widget(int i) {
